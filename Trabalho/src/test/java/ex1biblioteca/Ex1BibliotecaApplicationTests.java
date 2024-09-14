5e4888659795f94323d945e9297a3695735a85a9
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2024-09-14 19:50:52 -0300

    Create Ex1BibliotecaApplicationTests.java

---
+++ Trabalho/src/test/java/ex1biblioteca/Ex1BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package ex1biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
