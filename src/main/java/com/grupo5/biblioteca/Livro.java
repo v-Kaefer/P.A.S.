7989a9d2c0f76349c67d93506a47d0eebefa9c4a
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2024-09-15 14:56:25 -0300

    Padrão Finalmente
    Finalmente essa merda compila.
    Falta:
     -[ ] Add dependência PostgreSQL
     -[ ] Add conexão com Neon db/endpoint

--- src/main/java/com/grupo5/biblioteca/Livro.java
+++ src/main/java/com/grupo5/biblioteca/Livro.java
@@ -1,8 +1,6 @@
-package com.grupo5.biblioteca.model;
+package com.grupo5.biblioteca;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class Livro {
     private long id;
     private String titulo;
--- src/test/java/com/grupo5/biblioteca/BibliotecaApplicationTests.java
+++ src/test/java/com/grupo5/biblioteca/BibliotecaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bcopstein.ex1biblioeca;
+package com.grupo5.biblioteca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
